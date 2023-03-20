e60f45aef3820c43cf5056478138e8f892967c99
Mradul Varshney <93596474+KroNicalKODER@users.noreply.github.com>
2023-03-20 19:21:03 +0530

    Update tailwind.config.js

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,6 +4,13 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    screens : {
+      'sm' : {'max' : '640px'},
+      'md': {'max' : '768px'},
+      'lg': {'max' : '1024px'},
+      'xl': {'max' : '1280px'},
+      '2xl': {'max' : '1536px'},
+    },
     extend: {},
   },
   plugins: [],
