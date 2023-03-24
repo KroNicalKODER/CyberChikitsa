dfd9a509d85a0982782b2a7a92fd7e75b26ee2b1
Shivansh-25 <89247605+Shivansh-25@users.noreply.github.com>
2023-03-24 14:45:39 +0530

    first commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 import './App.css';
+import Register from './Pages/register';
 
 function App() {
   return (
     <div className="">
-      App
-      <motion.div className='w-44 h-44 bg-black' animate={{ x: 100 }} />
+      {/* App */}
+      <Register/>
+      {/* <motion.div className='w-44 h-44 bg-black' animate={{ x: 100 }} /> */}
     </div>
   );
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,17 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    screens : {
-      'sm' : {'max' : '640px'},
-      'md': {'max' : '768px'},
-      'lg': {'max' : '1024px'},
-      'xl': {'max' : '1280px'},
-      '2xl': {'max' : '1536px'},
+    screens: {
+      sm: { max: "640px" },
+      md: { max: "768px" },
+      lg: { max: "1024px" },
+      xl: { max: "1280px" },
+      "2xl": { max: "1536px" },
     },
-    extend: {},
+    extend: {
+      colors: {
+        "background": '#200040',
+        "grad-left": '#984900',
+        "grad-right": '#FFA149',
+        "purple-btn": '#830098',
+      },
+      gridTemplateColumns: {
+        'custom': '2fr 3fr',
+      },
+    },
   },
   plugins: [],
-}
+};
